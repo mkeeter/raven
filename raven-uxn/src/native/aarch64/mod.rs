850aafe55e080252da835b999c2c4aa508277948
Matt Keeter <matt.j.keeter@gmail.com>
2026-03-07 14:18:45 +0000

    Consolidate jump table

--- raven-uxn/src/native/aarch64/mod.rs
+++ raven-uxn/src/native/aarch64/mod.rs
@@ -2,10 +2,12 @@
 core::arch::global_asm!(concat!(
     include_str!("macos.s"),
     include_str!("impl.s")
+    include_str!("../jump_table.s"),
 ));
 
 #[cfg(target_os = "linux")]
 core::arch::global_asm!(concat!(
     include_str!("linux.s"),
     include_str!("impl.s")
+    include_str!("../jump_table.s"),
 ));
--- raven-uxn/src/native/x86_64/mod.rs
+++ raven-uxn/src/native/x86_64/mod.rs
@@ -1,5 +1,6 @@
 #[cfg(target_os = "linux")]
 core::arch::global_asm!(concat!(
     include_str!("linux.s"),
-    include_str!("impl.s")
+    include_str!("impl.s"),
+    include_str!("../jump_table.s"),
 ));
